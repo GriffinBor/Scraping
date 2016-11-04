5b79086aef4202844a56ca063c9e75dcab0e09ec
GriffinBor <griff-b@hotmail.com>
2016-11-03 20:09:33 -0500

    Griff - New base

--- main.py
+++ main.py
@@ -5,22 +5,26 @@ import Analyzer
 import Trader
 import Manager
 import matplotlib.pyplot as plt
-
+import time
+start_time = time.time()
 
 ticker_list = ['AAPL', 'TWTR']
 
 manager_accounts = {}
 ticker_services = {}
 
-for ticker in ticker_list:
-    ticker_services[ticker] = {'Scraper': Scraper.Ticker_Scraper(ticker), 'Analyzer': Analyzer.Ticker_Analzyer(ticker), 'Trader': Trader.Ticker_Trader(ticker)}
+for ticker in scraping_settings.TICKER_LIST:
+    ticker_services[ticker] = {'Scraper': Scraper.Ticker_Scraper(ticker), 'Analyzer': Analyzer.Ticker_Analzyer(ticker),
+                               'Trader': Trader.Ticker_Trader(ticker)}
 
-for ticker in ticker_services:
-    cur_scraper = ticker_services[ticker]['Scraper']
-    cur_trader = ticker_services[ticker]['Trader']
-    cur_analyzer = ticker_services[ticker]['Analyzer']
-    data_list = []
-    for item in cur_scraper.get_closing_price_list():
-        print(item)
+# for ticker in ticker_services:
+#     cur_scraper = ticker_services[ticker]['Scraper']
+#     cur_trader = ticker_services[ticker]['Trader']
+#     cur_analyzer = ticker_services[ticker]['Analyzer']
+#     data_list = []
+#     for item in cur_scraper.get_closing_price_list():
+#         print(item)
+#
 
-
+print('\n\n\n')
+print("--- %s seconds ---" % (time.time() - start_time))
